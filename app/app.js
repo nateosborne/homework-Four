8dc5243eb9236727f746bd54038f9a12abbb92d6
nateosborne <natedresdeno@gmail.com>
2022-09-26 10:05:29 -0400

    Changes the sweet alert

--- app/app.js
+++ app/app.js
@@ -4,23 +4,24 @@ $(document).ready(function() {
 
 $(window).on("load", function(){
     console.log("loaded");
-    Swal.fire({
-        title: '<strong>HTML <u>example</u></strong>',
-        icon: 'info',
-        html:
-          'You can use <b>bold text</b>, ' +
-          '<a href="//sweetalert2.github.io">links</a> ' +
-          'and other HTML tags',
-        showCloseButton: true,
-        showCancelButton: true,
-        focusConfirm: false,
-        confirmButtonText:
-          '<i class="fa fa-thumbs-up"></i> Great!',
-        confirmButtonAriaLabel: 'Thumbs up, great!',
-        cancelButtonText:
-          '<i class="fa fa-thumbs-down"></i>',
-        cancelButtonAriaLabel: 'Thumbs down'
-      })
-    $(".modal").css("display","none");
+    Swal.fire("Any fool can use a computer");
+    // Swal.fire({
+    //     title: '<strong>HTML <u>example</u></strong>',
+    //     icon: 'info',
+    //     html:
+    //       'You can use <b>bold text</b>, ' +
+    //       '<a href="//sweetalert2.github.io">links</a> ' +
+    //       'and other HTML tags',
+    //     showCloseButton: true,
+    //     showCancelButton: true,
+    //     focusConfirm: false,
+    //     confirmButtonText:
+    //       '<i class="fa fa-thumbs-up"></i> Great!',
+    //     confirmButtonAriaLabel: 'Thumbs up, great!',
+    //     cancelButtonText:
+    //       '<i class="fa fa-thumbs-down"></i>',
+    //     cancelButtonAriaLabel: 'Thumbs down'
+    //   })
+    // $(".modal").css("display","none");
 });
 
